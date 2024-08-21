e18fc6d0d5f772ada1bf427099a1db29894240be
PV Anfas <anfaspv.info@gmail.com>
2024-08-21 08:47:15 +0530

    up

--- main/context_processors.py
+++ main/context_processors.py
@@ -9,7 +9,6 @@ def main_context(request):
     if request.user.is_authenticated:
         current_user = request.user
         current_user.first_name
-
     return {
         "current_user": current_user,
         "default_user_avatar": "/static/app/config/default_user_avatar.jpeg",
@@ -19,4 +18,6 @@ def main_context(request):
         "company_address": org_data.get("company_address"),
         "company_mobile": org_data.get("company_mobile"),
         "company_mail": org_data.get("company_mail"),
+        "site_title": settings.APP_SETTINGS.get("site_title"),
+        "site_description": settings.APP_SETTINGS.get("site_description"),
     }
